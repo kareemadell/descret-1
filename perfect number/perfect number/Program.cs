dc20c80fef10ca52a188d135c5be1b1058d0d767
kareemadell <114925509+kareemadell@users.noreply.github.com>
2022-12-26 20:17:35 +0200

    Add files via upload

---
+++ perfect number/perfect number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter frist number");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter ende number");
+            double num2 = double.Parse(Console.ReadLine());
+            if (num1 < num2)
+            {
+                for (double i = num1; i <= num2; i++)
+                {
+                    double sum = 0;
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (i % j == 0) sum += j;
+                    }
+                    if (sum == i) Console.WriteLine("number =  " + i);
+
+
+
+                }
+
+
+
+            }
+            else Console.WriteLine("you have error");
+        }
+    }
+}
